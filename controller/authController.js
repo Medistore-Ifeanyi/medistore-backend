370af700a739af8f2e95a7af15d4542a5c607396
Charles <adeakinyemi129@gmail.com>
2025-11-07 11:34:06 +0100

    admin

--- controller/authController.js
+++ controller/authController.js
@@ -32,55 +32,30 @@ export const registerUser = async (req, res) => {
 
 
 
-// export const loginUser = async (req, res) => {
-  
-//   try {
-
-//     let { email, password } = req.body;
-
-//     email = email.trim().toLowerCase()
-
-
-//     const user = await User.findOne({ email });
-//     if (!user)
-//       return res.status(400).json({ message: "Invalid Login Credentials" });
-//     const isMatch = await bcrypt.compare(password, user.password);
-//     if (!isMatch)
-//       return res.status(400).json({ message: "Invalid Login Credentials" });
-//     // include role in response
-//     res.json({
-//       _id: user._id,
-//       name: user.name,
-//       email: user.email,
-//       role: user.role,
-//       token: generateToken(user._id, user.role, user.email), // Pass role to token generator if desired
-//     });
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// };
-
-
 export const loginUser = async (req, res) => {
+
+  const { email, password } = req.body;
+  
   try {
-    let { email, password } = req.body;
-    // Normalize email to lowercase and trim spaces
-    email = email.trim().toLowerCase();
+
+
     const user = await User.findOne({ email });
     if (!user)
       return res.status(400).json({ message: "Invalid Login Credentials" });
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch)
       return res.status(400).json({ message: "Invalid Login Credentials" });
-    // Successful login — return token and user info
+    // include role in response
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
       role: user.role,
-      token: generateToken(user._id, user.role, user.email),
+      token: generateToken(user._id, user.role, user.email), // Pass role to token generator if desired
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+
